9c3c41d91587a2ccc8be70b39da4075686f60f7f
Earth287 <erin007ster@gmail.com>
2016-10-03 13:36:20 -0400

    Methods and loop working properly. Need to create a loop to keep the program running until user quits and account for plurality in hash items

--- inventory_EA.rb
+++ inventory_EA.rb
@@ -1,63 +1,55 @@
-
-
-def update_animal
-  zoo = {}
-  zoo = {"lions"=> 5, "bears"=>6, "elephants"=>10, "wolves"=>20, "kangaroos"=>17}
-puts "What animal would you like to update?"
-animal = gets.chomp.downcase
-animal_list = zoo.keys
-while animal_list.include?(animal) != true
-puts "That is not one of the animals in the inventory, please choose an animal that is in the inventory list."
-animal = gets.chomp.downcase
-end
-puts "How many #{animal} are there now?"
-new_number = gets.chomp.to_i
-puts "Thank you, I will update the inventory."
-
-zoo[animal] = new_number
-puts zoo
+def update_animal (a)
+  puts "What animal would you like to update?"
+  animal = gets.chomp.downcase
+  animal_list = a.keys
+    while animal_list.include?(animal) != true
+      puts "That is not one of the animals in the inventory, please choose an animal that is in the inventory list."
+      animal = gets.chomp.downcase
+    end
+  puts "How many #{animal} are there now?"
+  new_number = gets.chomp.to_i
+  puts "Thank you, the inventory has been updated, please see below."
+  a[animal] = new_number
+  puts a
 end
 
-def add_animal
-  zoo = {}
-  zoo = {"lions"=> 5, "bears"=>6, "elephants"=>10, "wolves"=>20, "kangaroos"=>17}
-puts "What is the new animal that you need to add?"
-new_animal = gets.chomp.downcase
-animal_list = zoo.keys
-puts "How many #{new_animal} are there?"
-new_count = gets.chomp.downcase
-
-zoo[new_animal] = new_count
-puts zoo
+def add_animal (a)
+  puts "What is the new animal that you need to add?"
+  new_animal = gets.chomp.downcase
+  animal_list = a.keys
+  puts "How many #{new_animal} are there?"
+  new_count = gets.chomp.to_i
+  a[new_animal] = new_count
+  puts "Thank you, #{new_animal} has been added. Please see the updated inventory below."
+  puts a
 end
 
 
-def remove_animal
-  zoo = {}
-  zoo = {"lions"=> 5, "bears"=>6, "elephants"=>10, "wolves"=>20, "kangaroos"=>17}
-puts "What is the animal that you would like to remove?"
-delete_animal = gets.chomp.downcase
-animal_list = zoo.keys
-while animal_list.include?(delete_animal) != true
-puts "That is not one of the animals in the inventory, please choose an animal that is in the inventory list."
-delete_animal = gets.chomp.downcase
-end
-zoo.delete(delete_animal)
-puts "Thank you, #{delete_animal} has been removed."
-puts zoo
+def remove_animal (a)
+  puts "What is the animal that you would like to remove?"
+  delete_animal = gets.chomp.downcase
+  animal_list = a.keys
+    while animal_list.include?(delete_animal) != true
+        puts "That is not one of the animals in the inventory, please choose an animal that is in the inventory list."
+        delete_animal = gets.chomp.downcase
+    end
+  a.delete(delete_animal)
+  puts "Thank you, #{delete_animal} has been removed. Please see the updated inventory below."
+  puts a
 end
 
 zoo = {}
 zoo = {"lions"=> 5, "bears"=>6, "elephants"=>10, "wolves"=>20, "kangaroos"=>17}
-puts "Here is your current inventory"
+
+puts "Here is your current inventory:"
 puts zoo
 puts "Would you like to update, add, or remove an item in the inventory?"
 choice = gets.chomp.downcase
 
 if choice == "add"
-  add_animal
+  add_animal (zoo)
 elsif choice == "update"
-  update_animal
+  update_animal (zoo)
 else
-  remove_animal
+  remove_animal (zoo)
 end
